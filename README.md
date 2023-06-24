c64029356f10f3bb6ed9dbc9cfb6629e01e93afe
kyoh86 <me@kyoh86.dev>
2023-06-24 22:31:09 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ddu-filter-converter_hi_dir
+
+ddu.vim converter that highlights directory path of file-like items.
+
+## screenshot
+
+before:
+<img width="221" alt="24e2d808e093c1e9f23f137251cef627" src="https://github.com/kyoh86/ddu-filter-converter_hl_dir/assets/5582459/e7da5ce4-af97-492c-b1ba-b17272fd9274">
+
+after:
+<img width="223" alt="3c1e46b813e9bddfb7b5eb442d1c74b8" src="https://github.com/kyoh86/ddu-filter-converter_hl_dir/assets/5582459/bf3648a2-5f33-4c86-b693-ed972b3c511a">
---
+++ denops/@ddu-filters/converter_hl_dir.ts
@@ -0,0 +1,69 @@
+import {
+  BaseFilter,
+  DduItem,
+} from "https://deno.land/x/ddu_vim@v3.2.6/types.ts";
+import { basename, dirname } from "https://deno.land/std@0.192.0/path/mod.ts";
+import { is } from "https://deno.land/x/unknownutil@v3.2.0/mod.ts";
+
+const HIGHLIGHT_NAME = "ddu_dir";
+const ENCODER = new TextEncoder();
+
+type Params = {
+  hlGroup: string;
+};
+
+function getPath(item: DduItem): string | undefined {
+  if (is.ObjectOf({ action: is.ObjectOf({ path: is.String }) })(item)) {
+    return item.action.path;
+  }
+}
+
+export class Filter extends BaseFilter<Params> {
+  filter(args: {
+    filterParams: Params;
+    items: DduItem[];
+  }): Promise<DduItem[]> {
+    const { filterParams, items } = args;
+    return Promise.resolve(items.map((item) => {
+      const path = getPath(item);
+      if (!path) {
+        return item;
+      }
+
+      const { word, display = word, highlights = [] } = item;
+
+      // Already highlighted
+      if (highlights.some((item) => item.name === HIGHLIGHT_NAME)) {
+        return item;
+      }
+
+      // A path without parent
+      const base = basename(display);
+      if (base == display) {
+        return item;
+      }
+
+      const dir = dirname(display);
+      const col = 1;
+      const width = ENCODER.encode(dir).length + 1;
+
+      item.highlights = [
+        ...highlights,
+        {
+          name: HIGHLIGHT_NAME,
+          col,
+          width,
+          hl_group: filterParams.hlGroup,
+        },
+      ];
+
+      return item;
+    }));
+  }
+
+  params(): Params {
+    return {
+      hlGroup: "Directory",
+    };
+  }
+}

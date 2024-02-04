e45755e20d423242136f7a0b2b0cb12744f3d2bb
kyoh86 <me@kyoh86.dev>
2024-02-05 08:48:06 +0900

    Update dependencies

--- dummy_test.ts
+++ dummy_test.ts
@@ -1,5 +1,5 @@
-import { test } from "https://deno.land/x/denops_test@v1.6.1/mod.ts";
-import { assert } from "https://deno.land/std@0.212.0/assert/mod.ts";
+import { test } from "https://deno.land/x/denops_test@v1.6.2/mod.ts";
+import { assert } from "https://deno.land/std@0.214.0/assert/mod.ts";
 
 test({
   mode: "all",

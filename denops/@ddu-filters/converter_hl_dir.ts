6ce20739890723f61fcc4b3ce365e9923c797bfb
kyoh86 <me@kyoh86.dev>
2024-04-11 21:37:01 +0900

    Update dependencies

--- denops/@ddu-filters/converter_hl_dir.ts
+++ denops/@ddu-filters/converter_hl_dir.ts
@@ -2,7 +2,7 @@ import {
   BaseFilter,
   DduItem,
 } from "https://deno.land/x/ddu_vim@v3.10.3/types.ts";
-import { SEPARATOR_PATTERN } from "https://deno.land/std@0.221.0/path/mod.ts";
+import { SEPARATOR_PATTERN } from "https://deno.land/std@0.222.1/path/mod.ts";
 import { is } from "https://deno.land/x/unknownutil@v3.17.2/mod.ts";
 
 const HIGHLIGHT_NAME = "ddu_dir";

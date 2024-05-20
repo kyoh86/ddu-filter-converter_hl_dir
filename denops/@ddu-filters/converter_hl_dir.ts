9621479a793fd716ab731be6d7adee127c747202
kyoh86 <me@kyoh86.dev>
2024-05-20 22:44:10 +0900

    Update dependencies

--- denops/@ddu-filters/converter_hl_dir.ts
+++ denops/@ddu-filters/converter_hl_dir.ts
@@ -1,9 +1,9 @@
 import {
   BaseFilter,
   DduItem,
-} from "https://deno.land/x/ddu_vim@v4.0.0/types.ts";
-import { SEPARATOR_PATTERN } from "https://deno.land/std@0.223.0/path/mod.ts";
-import { is } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
+} from "https://deno.land/x/ddu_vim@v4.1.0/types.ts";
+import { SEPARATOR_PATTERN } from "https://deno.land/std@0.224.0/path/mod.ts";
+import { is } from "https://deno.land/x/unknownutil@v3.18.1/mod.ts";
 
 const HIGHLIGHT_NAME = "ddu_dir";
 const ENCODER = new TextEncoder();
--- dummy_test.ts
+++ dummy_test.ts
@@ -1,5 +1,5 @@
-import { test } from "https://deno.land/x/denops_test@v1.6.2/mod.ts";
-import { assert } from "https://deno.land/std@0.223.0/assert/mod.ts";
+import { test } from "https://deno.land/x/denops_test@v1.8.0/mod.ts";
+import { assert } from "https://deno.land/std@0.224.0/assert/mod.ts";
 
 test({
   mode: "all",
